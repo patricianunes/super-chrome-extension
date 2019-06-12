198682097565032f6db99d882c980a54399bff7b
Patricia Campos <patricia@krint.io>
2019-06-12 18:53:43 +1000

    put the app outside from the extension and add in the link

--- src/components/Delicious.js
+++ src/components/Delicious.js
@@ -1,47 +1,13 @@
-import React, {Component} from 'react';
+import React from "react";
+import Image from "../recipe.png";
 
-import '../Delicious.css';
-import DeliciousForm from './DeliciousForm';
-import DeliciousRecipes from './DeliciousRecipes';
-
-//const API_KEY = "88644a2a377f68746cc0267e10914cf2";
-const API_KEY = "f13fd82da7f916744adb750f28b4b4f6"
-
-class Delicious extends Component {
-  state = {
-    recipes:[]
-  }
-
-  getRecipe = async (e) => {
-    const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`);
-    const data = await api_call.json();
-    this.setState({recipes: data.recipes});
-  }
-
-  componentDidMount = () => {
-    const json = localStorage.getItem("recipes");
-    const recipes = JSON.parse(json);
-    this.setState({recipes})
-  }
-
-  componentDidUpdate = () => {
-    const recipes = JSON.stringify(this.state.recipes);
-    localStorage.setItem("recipes", recipes);
-  }
-
-  render() {
-    return(
-      <div className="App-delicious">
-        <header className="App-delicious-header">
-          <h1 className="App-delicious-title">Delicious Recipes</h1>
-        </header>
-        <DeliciousForm getRecipe={this.getRecipe}/>
-        <DeliciousRecipes recipes={this.state.recipes}/>
-      </div>
-    );
-  }
-}
+const Delicious = () => (
+  <a
+    className="App-link"
+    href="https://patricianunes.github.io/delicious-recipes/#/"
+  >
+    <img alt="DeliciousRecipes" src={Image} /> Delicious Recipes
+  </a>
+);
 
 export default Delicious;
