a2008bf682297352c22c8bae5610c0ee16f7b3b5
Patricia Campos <patricia@krint.io>
2019-06-14 13:42:10 +1000

    Updates

---
+++ precache-manifest.cb54fc1dcf52f030cee9912201875b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8707568dbf6a17d638cbcb91fbd55c",
+    "url": "/super-chrome-extension/index.html"
+  },
+  {
+    "revision": "5bb5a16ef9d37603ac51",
+    "url": "/super-chrome-extension/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fc670707144fa8c0a7d8",
+    "url": "/super-chrome-extension/static/css/main.36325b7a.chunk.css"
+  },
+  {
+    "revision": "5bb5a16ef9d37603ac51",
+    "url": "/super-chrome-extension/static/js/2.1db26245.chunk.js"
+  },
+  {
+    "revision": "fc670707144fa8c0a7d8",
+    "url": "/super-chrome-extension/static/js/main.021750a2.chunk.js"
+  },
+  {
+    "revision": "5e61da62c004d4f47066",
+    "url": "/super-chrome-extension/static/js/runtime~main.44eac186.js"
+  }
+]);
